417431d3d82c6c9d45ce1dcd65565e8ce7038bbf
8c_6t <nkw8620@gmail.com>
2019-05-23 01:34:37 +0900

    add: 페이스북 크롤링 준비

---
+++ ch07/1_facebook/index.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const dotenv = require('dotenv');
+
+const db = require('./models');
+
+dotenv.config();
+
+const crawler = async () => {
+  try {
+    await db.sequelize.sync(); 
+
+    const browser = await puppeteer.launch({
+      headless: process.env.NODE_ENV === 'production',
+      args: ['--window-size=1920,1080', '--disable-notifications']
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1080,
+      height: 1080,
+    });
+
+    await page.goto('https://facebook.com');
+
+    const id = process.env.FB_ID;
+    const pw = process.env.FB_PW;
+
+    await page.type('#email', id);
+    await page.type('#pass', pw);
+
+    await page.click('#loginbutton');
+/* 
+    await page.waitForResponse((response) => {
+      return response.url().includes('login_attempt');
+    });
+ */
+    await page.keyboard.press('Escape');
+
+
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+crawler();
