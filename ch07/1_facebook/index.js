5c951bad0cd88f6b59fdababcac92f688d3ee0f8
8c_6t <nkw8620@gmail.com>
2019-05-23 17:16:21 +0900

    update: 이미지 선택자 수정, 반복문 추가

--- ch07/1_facebook/index.js
+++ ch07/1_facebook/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const crawler = async () => {
   try {
-    // await db.sequelize.sync(); 
+    await db.sequelize.sync(); 
 
     const browser = await puppeteer.launch({
       headless: process.env.NODE_ENV === 'production',
@@ -39,40 +39,45 @@ const crawler = async () => {
     await page.waitForSelector('#userNavigationLabel');
     await page.keyboard.press('Escape');
 
-    await page.waitForSelector('[id^=hyperfeed_story_id]:first-child .userContentWrapper');
+    let result = [];
+    while(result.length < 10) {
+      await page.waitForSelector('[id^=hyperfeed_story_id]:first-child .userContentWrapper');
 
-    // '[id^=hyperfeed_story_id]:not([class*="sponsored_ad"])'
-    const newPost = await page.evaluate(() => {
-      const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
-      const name = firstFeed.querySelector('.fwb.fcg') && firstFeed.querySelector('.fwb.fcg').textContent;
-      const content = firstFeed.querySelector('.userContent') && firstFeed.querySelector('.userContent').textContent;
-      const img = firstFeed.querySelectorAll('[class=mtm]') && document.querySelectorAll('[class=mtm] img').src;
-      const postId = firstFeed.id.split('_').slice(-1)[0];
+      // '[id^=hyperfeed_story_id]:not([class*="sponsored_ad"])'
+      const newPost = await page.evaluate(() => {
+        const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
+        const name = firstFeed.querySelector('.fwb.fcg') && firstFeed.querySelector('.fwb.fcg').textContent;
+        const content = firstFeed.querySelector('.userContent') && firstFeed.querySelector('.userContent').textContent;
+        const img = firstFeed.querySelector('[class=mtm]') && document.querySelector('[class=mtm] img').src;
+        const postId = firstFeed.id.split('_').slice(-1)[0];
 
-      return { name, content, postId, img }
-    });
+        return { name, content, postId, img }
+      });
 
-    console.log(newPost);
+      console.log(newPost);
+      result.push(newPost);
 
+      await page.waitFor(1000);
+      const likeBtn = await page.$('[id^=hyperfeed_story_id]:first-child ._666k a');
+
+      await page.evaluate((like) => {
+        const sponsor = document.querySelector('[id^=hyperfeed_story_id]:first-child[class*="sponsored_ad"]');
+        if(!sponsor && like.getAttribute('aria-pressed') === 'false') {
+          like.click();
+        } else if (sponsor && like.getAttribute('aria-pressed') === 'true') {
+          like.click();
+        }
+      }, likeBtn);
+
+      await page.waitFor(1000);
+
+      await page.evaluate(() => {
+        const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
+        firstFeed.parentNode.removeChild(firstFeed);
+      });
+      
     await page.waitFor(1000);
-    const likeBtn = await page.$('[id^=hyperfeed_story_id]:first-child ._666k a');
-
-    await page.evaluate((like) => {
-      const sponsor = document.querySelector('[id^=hyperfeed_story_id]:first-child[class*="sponsored_ad"]');
-      if(!sponsor && like.getAttribute('aria-pressed') === 'false') {
-        like.click();
-      } else if (sponsor && like.getAttribute('aria-pressed') === 'true') {
-        like.click();
-      }
-    }, likeBtn);
-
-    await page.waitFor(1000);
-
-    await page.evaluate(() => {
-      const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
-      firstFeed.parentNode.removeChild(firstFeed);
-    });
-    await page.waitFor(1000);
+    }
 
   } catch (error) {
     console.error(error);
